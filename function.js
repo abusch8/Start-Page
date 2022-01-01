c8ff747ace97d46313423fb0b915eeee55f8f8b3
Alex Busch <abusch8@huskers.unl.edu>
2022-01-01 16:45:49 -0600

    Simplified date and time code, began css styling

--- function.js
+++ function.js
@@ -4,47 +4,8 @@ updateClock();
 function updateClock() {
     const date = new Date();
     document.getElementById('welcome-msg').textContent = 'Good ' + ((date.getHours() <= 11) ? 'morning ' : 'evening ') + userName + '!';
-    let month = undefined;
-    switch (date.getMonth()) {
-        case 0:
-            month = 'January';
-            break;
-        case 1:
-            month = 'Feburary';
-            break;
-        case 2:
-            month = 'March';
-            break;
-        case 3:
-            month = 'April';
-            break;
-        case 4:
-            month = 'May';
-            break;
-        case 5:
-            month = 'June';
-            break;
-        case 6:
-            month = 'July';
-            break;
-        case 7:
-            month = 'August';
-            break;
-        case 8:
-            month = 'September';
-            break;
-        case 9:
-            month = 'October';
-            break;
-        case 10:
-            month = 'November';
-            break;
-        case 11:
-            month = 'December';
-            break;
-    }
-    document.getElementById('date').textContent = month + ' ' + date.getDate() + ((date.getDate() === 1) ? 'st, ' : 'th, ') + date.getFullYear();
-    document.getElementById('time').textContent = date.getHours() + ':' + String(date.getMinutes()).padStart(2, '0') + ':' + String(date.getSeconds()).padStart(2, '0') + ((date.getHours() <= 11) ? ' AM' : ' PM');
+    document.getElementById('time').textContent = date.toLocaleTimeString('en-US', {hour12: true});
+    document.getElementById('date').textContent = date.toLocaleDateString('en-US', {dateStyle: 'full'});
 }   
 setInterval(updateClock, 1000);
 
