6c365d016ebfc8f83aecd0e4e5f1197037d5c32f
Alex Busch <abusch8@huskers.unl.edu>
2022-01-10 19:28:21 -0600

    Cleaned up task list code, began implementing settings panel

--- function.js
+++ function.js
@@ -1,9 +1,12 @@
-const userName = 'Alex';
+if (localStorage.getItem('userName') === null) {
+    localStorage.setItem('userName', JSON.stringify('User'));
+}
+const userName = JSON.parse(localStorage.getItem('userName'));
 
 updateClock();
 function updateClock() {
     const date = new Date();
-    document.getElementById('welcome-msg').textContent = 'Good ' + ((date.getHours() <= 11) ? 'morning ' : 'evening ') + userName + '!';
+    document.getElementById('welcome_msg').textContent = 'Good ' + ((date.getHours() <= 11) ? 'morning ' : 'evening ') + userName + '!';
     document.getElementById('time').textContent = date.toLocaleTimeString('en-US', {hour12: true});
     document.getElementById('date').textContent = date.toLocaleDateString('en-US', {dateStyle: 'full'});
 }   
@@ -18,19 +21,19 @@ renderList();
 
 let editing = false;
 
-const addTextField = document.getElementById('add-field');
+const addTextField = document.getElementById('add_field');
 addTextField.addEventListener('keypress', function(ev) {
     if (ev.key === 'Enter') {
         addToList();
     }
 });
 
-document.getElementById('add-btn').addEventListener('click', addToList);
+document.getElementById('add_btn').addEventListener('click', addToList);
 
-document.getElementById('sort-btn').addEventListener('click', function() {
+document.getElementById('sort_btn').addEventListener('click', function() {
     tasks.sort((a, b) => {
         if ((a.completed && b.completed) || (!a.completed && !b.completed)) {
-            return a.str.localeCompare(b.str);
+            return a.value.localeCompare(b.value);
         }
         return (a.completed) ? 1 : -1;
     });
@@ -38,7 +41,7 @@ document.getElementById('sort-btn').addEventListener('click', function() {
     renderList();
 });
 
-document.getElementById('clear-btn').addEventListener('click', function() {
+document.getElementById('clear_btn').addEventListener('click', function() {
     if (confirm("Are you sure you want to clear the list?")) {
         editing = false;
         tasks = [];
@@ -47,8 +50,8 @@ document.getElementById('clear-btn').addEventListener('click', function() {
     }
 });
 
-function Task(str) {
-    this.str = str;
+function Task(value) {
+    this.value = value;
     this.completed = false;
 }
 
@@ -62,109 +65,108 @@ function addToList() {
 }
 
 function renderList() {
-    document.getElementById('task-list').innerText = '';
+    document.getElementById('task_list').innerText = '';
     for (let i = 0; i < tasks.length; ++i) {
-        const container = document.createElement('div');
-        container.setAttribute('class', 'li-container');
+        const taskContainer = document.createElement('div');
+        taskContainer.setAttribute('class', 'task-container');
 
-        const outerDiv = document.createElement('div');
-        outerDiv.setAttribute('class', 'li-wrapper');
+        const liWrapper = document.createElement('div');
+        liWrapper.setAttribute('class', 'li-wrapper');
 
-        const innerDiv = document.createElement('div');
-        innerDiv.innerText = tasks[i].str;
-        innerDiv.setAttribute('draggable', true);
-        innerDiv.setAttribute('ondragstart', 'drag(event)');
-        innerDiv.setAttribute('ondrop', 'drop(event)');
-        innerDiv.setAttribute('ondragover', 'allowDrop(event)');
-        innerDiv.setAttribute('id', 'task-' + String(i).padStart(3, '0'));
-        innerDiv.addEventListener('click', function() {
+        const li = document.createElement('li');
+
+        const task = document.createElement('div');
+        task.setAttribute('id', 'task_' + String(i).padStart(3, '0'));
+        task.setAttribute('class', (tasks[i].completed) ? 'task-completed' : 'task');
+        task.setAttribute('draggable', true);
+        task.setAttribute('ondragstart', 'drag(event)');
+        task.setAttribute('ondrop', 'drop(event)');
+        task.setAttribute('ondragover', 'allowDrop(event)');
+        task.innerText = tasks[i].value;
+        task.addEventListener('click', function() {
             if (!editing) {
-                console.log(this.id.slice(this.id.length - 3));
                 tasks[Number(this.id.slice(this.id.length - 3))].completed = !tasks[Number(this.id.slice(this.id.length - 3))].completed;
                 localStorage.setItem('tasks', JSON.stringify(tasks));
                 renderList();
             }
         });
 
-        const li = document.createElement('li');
-        if (tasks[i].completed) {
-            li.setAttribute('class', 'completed');
-        }
-        li.appendChild(innerDiv);
-        outerDiv.appendChild(li)
-        container.appendChild(outerDiv);
+        li.appendChild(task);
+        liWrapper.appendChild(li);
+        taskContainer.appendChild(liWrapper);
+
+        const popUpContainer = document.createElement('div');
+        popUpContainer.setAttribute('class', 'btn-popup-container');
 
         const editButton = document.createElement('button');
-        editButton.setAttribute('id', 'edit-btn-' + String(i).padStart(3, '0'));
+        editButton.setAttribute('type', 'button');
         editButton.setAttribute('class', 'edit-btn');
         editButton.innerText = 'Edit';
         editButton.addEventListener('click', function() {
             editing = true;
-            li.setAttribute('draggable', false);
-            editTextField.value = li.innerText;
-            innerDiv.style.display = 'none';
-            editButton.style.display = 'none';
-            deleteButton.style.display = 'none';
-            editTextField.style.display = 'inline-block';
-            confirmButton.style.display = 'inline-block';
-            editTextField.select();
+            editField.value = task.innerText;
+            task.style.display = 'none';
+            popUpContainer.style.display = 'none';
+            editContainer.style.display = 'inline';
+            editField.select();
         });
-        container.appendChild(editButton);
+        popUpContainer.appendChild(editButton);
 
         const deleteButton = document.createElement('button');
-        deleteButton.setAttribute('id', 'delete-btn-' + String(i).padStart(3, '0'));
+        deleteButton.setAttribute('type', 'button');
         deleteButton.setAttribute('class', 'delete-btn');
         deleteButton.innerText = 'Delete';
         deleteButton.addEventListener('click', function() {
-            tasks.splice(this.id.slice(this.id.length - 3), 1);
+            tasks.splice(task.id.slice(task.id.length - 3), 1);
             localStorage.setItem('tasks', JSON.stringify(tasks));
             renderList();
         });
-        container.appendChild(deleteButton);
+        popUpContainer.appendChild(deleteButton);
+        taskContainer.appendChild(popUpContainer);
 
-        const editTextField = document.createElement('input');
-        editTextField.setAttribute('type', 'text');
-        editTextField.setAttribute('id', 'edit-field-' + String(i).padStart(3, '0'));
-        editTextField.setAttribute('class', 'edit-field');
-        editTextField.addEventListener('keypress', function(ev) {
+        const editContainer = document.createElement('div');
+        editContainer.setAttribute('class', 'edit-task-container');
+
+        const editField = document.createElement('input');
+        editField.setAttribute('type', 'text');
+        editField.setAttribute('class', 'edit-field');
+        editField.addEventListener('keypress', function(ev) {
             if (ev.key === 'Enter') {
                 modifyList();
             }
         });
-        li.appendChild(editTextField);
+        editContainer.appendChild(editField);
 
         const confirmButton = document.createElement('button');
-        confirmButton.setAttribute('id', 'confirm-btn-' + String(i).padStart(3, '0'));
+        confirmButton.setAttribute('type', 'submit');
         confirmButton.setAttribute('class', 'confirm-btn');
         confirmButton.innerText = 'Confirm';
         confirmButton.addEventListener('click', modifyList);
-        container.appendChild(confirmButton);
+        editContainer.appendChild(confirmButton);
+        li.appendChild(editContainer);
 
         function modifyList() {
-            if (editTextField.value !== '') {
+            if (editField.value !== '') {
                 editing = false;
-                editTextField.style.display = 'none';
-                confirmButton.style.display = 'none';
-                innerDiv.style.display = 'inline-block';
-                tasks[Number(editTextField.id.slice(editTextField.id.length - 3))].str = editTextField.value;
+                editContainer.style.display = 'none';
+                task.style.display = 'inline';
+                tasks[Number(task.id.slice(task.id.length - 3))].value = editField.value;
                 localStorage.setItem('tasks', JSON.stringify(tasks));
                 renderList();
             }
         }
 
-        container.addEventListener('mouseenter', function() {
+        taskContainer.addEventListener('mouseenter', function() {
             if (!editing) {
-                deleteButton.style.display = 'inline-block';
-                editButton.style.display = 'inline-block';
+               popUpContainer.style.display = 'inline';
             }
         });
 
-        container.addEventListener('mouseleave', function() {
-            deleteButton.style.display = 'none';
-            editButton.style.display = 'none';
+        taskContainer.addEventListener('mouseleave', function() {
+            popUpContainer.style.display = 'none';
         });
 
-        document.getElementById('task-list').appendChild(container);
+        document.getElementById('task_list').appendChild(taskContainer);
     }
 }
 
@@ -180,7 +182,6 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     if (dragId !== undefined && ev.target.id !== undefined) {
-        console.log(ev.target.id.slice(ev.target.id.length - 3));
         const swap = tasks[Number(ev.target.id.slice(ev.target.id.length - 3))];
         tasks[Number(ev.target.id.slice(ev.target.id.length - 3))] = tasks[dragId];
         tasks[dragId] = swap;
@@ -193,3 +194,20 @@ function drop(ev) {
 function allowDrop(ev) {
     ev.preventDefault();
 }
+
+const settingsMenu = document.getElementById('settings_menu');
+
+document.getElementById('settings_btn').addEventListener('click', function() {
+    settingsMenu.style.visibility = 'visible';
+});
+
+document.getElementById('close_btn').addEventListener('click', function() {
+    settingsMenu.style.visibility = 'hidden';
+});
+
+const userNameField = document.getElementById('user_name_field');
+userNameField.value = userName;
+
+document.getElementById('update_btn').addEventListener('click', function() {
+    localStorage.setItem('userName', JSON.stringify(userNameField.value));
+});
